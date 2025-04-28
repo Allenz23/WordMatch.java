c2c19d3c923034c8c7779eca4a8e8646f15ddd85
Allenz23 <allenz23@nycstudents.net>
2025-04-28 16:19:16 +0000

    all done

--- Main.java
+++ Main.java
@@ -7,6 +7,11 @@ public class Main{
         System.out.println(w.scoreGuess("mississippi"));
 
         WordMatch game = new WordMatch("concatenation");
-        System.out.println(game.scoreGuess(null));
+        System.out.println(game.scoreGuess("ten"));
+        System.out.println(game.scoreGuess("nation"));
+        System.out.println(game.findBetterGuess("ten", "nation"));
+        System.out.println(game.scoreGuess("con"));
+        System.out.println(game.scoreGuess("cat"));
+        System.out.println(game.findBetterGuess("con", "cat"));
     }
 }
